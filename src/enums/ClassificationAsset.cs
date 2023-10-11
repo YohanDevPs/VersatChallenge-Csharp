2ca3226279558750698901abe94c17da348cdc4e
YohanDevPs <yohanps.dev@gmail.com>
2023-10-10 23:00:17 -0300

    fix: fix error of class atribute

---
+++ src/enums/ClassificationAsset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudioCsharp.enums
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    sealed class ClassificationAsset : Attribute
+    {
+        public string Classification { get; }
+
+        public ClassificationAsset(string classification)
+        {
+            Classification = classification;
+        }
+    }
+}
